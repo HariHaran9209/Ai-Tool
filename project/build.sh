5e9d34eadba1907ff2156bddff4142e3208714eb
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-03-11 10:55:51 +0530

    Delete project directory

--- project/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-pip install -r requirements.txt
-python manage.py makemigrations 
-echo "Made Migrations!!!"
-python manage.py migrate
-echo "Migrated Successfully!!!"
