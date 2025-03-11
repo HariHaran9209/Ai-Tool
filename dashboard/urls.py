f4951ecb28cbad107368d6e55abb510079123f85
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-03-11 11:42:07 +0530

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,8 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='testing'),
-    path('og/', og, name='og'),
-    path('add_activity/', add_activity, name='add_activity'),
-    path('add_title/', add_title, name='add_title'),
-    path('add_section/', add_heading, name='add_section'),
+    path('main/', og, name='og'),
+    path('add-activity/', add_activity, name='add_activity'),
+    path('add-title/', add_title, name='add_title'),
+    path('add-section/', add_heading, name='add_section'),
+    path('delete-titles/<int:record_id>/', delete_titles, name='delete_titles'),
+    path('delete-headings/<int:record_id>/', delete_headings, name='delete_headings'),
+    path('delete-activities/<int:record_id>/', delete_activities, name='delete_activities'),
 ]
