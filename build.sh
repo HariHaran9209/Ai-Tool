f9749aa3ae6b648b845e1d6827d604ce52688677
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-03-11 11:20:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-set -o errexit  # Exit immediately if a command exits with a non-zero status
-set -o nounset  # Treat unset variables as an error when substituting
-set -o pipefail # The return value of a pipeline is the status of the last command to exit with a non-zero status
+#!/usr/bin/env bash
+set -o errexit
+
 pip install -r requirements.txt
+
 python manage.py makemigrations 
 echo "Made Migrations!!!"
+
 python manage.py migrate
 echo "Migrated Successfully!!!"
