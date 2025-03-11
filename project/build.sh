bd2635d07b066d2e8ce4e3a841dbcc26946fb4f8
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-03-11 10:54:25 +0530

    Add files via upload

---
+++ project/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -o errexit
+pip install -r requirements.txt
+python manage.py makemigrations 
+echo "Made Migrations!!!"
+python manage.py migrate
+echo "Migrated Successfully!!!"
